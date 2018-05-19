f8e1a4316f3169b67888d7b1b6d2b5954901d3e5
Aaron Gonzalez <Kabku43@gmail.com>
2018-05-19 00:01:36 -0400

    Adding logs to checked for socket connectivity

--- public/utils/client.js
+++ public/utils/client.js
@@ -3,8 +3,10 @@ $(document).ready( function(){
     let socket = io.connect('https://my-chatroom-backend-es.herokuapp.com:5000');
     let textbox = $('#my-message');
 
+    console.log("Checking if sockets are connected!")
+
     socket.on('connect', function(){
-        socket.send('User has connect!');
+        socket.send('User has connected! ', io.connected);
     });
 
     socket.on('message', function(msg){
