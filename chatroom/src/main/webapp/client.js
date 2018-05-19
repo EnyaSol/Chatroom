bbe71933c59a8edec5e56e7721b0b4eb7e1b7531
Aaron Gonzalez <Kabku43@gmail.com>
2018-05-18 20:41:15 -0400

    Moving directory & using create-react-app to setup react

--- chatroom/src/main/webapp/client.js
+++ chatroom/src/main/webapp/client.js
@@ -1,17 +1,19 @@
 $(document).ready( function(){
-    var socket = io.connect('http://127.0.0.1:5000');
+    let socket = io.connect('http://127.0.0.1:5000');
+    let textbox = $('#my-message');
 
     socket.on('connect', function(){
         socket.send('User has connect!');
-    })
+    });
 
     socket.on('message', function(msg){
         $("#messages").append('<li>' + msg + '<li>');
-        console.log('Received message')
-    })
+        console.log('Received message');
+    });
 
     $('#send-button').on('click', function(){
-        socket.send($('#my-message').val())
-        $('#my-message').val('')
+
+        socket.send(textbox.val());
+        textbox.val('');
     })
-})
+});
